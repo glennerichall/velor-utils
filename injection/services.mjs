1038389789caccc73f2752fe11ebb296822e539b
Glenn Hall <glennerichall@hotmail.com>
2024-11-09 10:11:31 -0500

    added logger holding service

--- injection/services.mjs
+++ injection/services.mjs
@@ -1,5 +1,8 @@
 import {getProvider} from "./baseServices.mjs";
-import {s_envNameResolver, s_logger} from "./serviceKeys.mjs";
+import {
+    s_envNameResolver,
+    s_logger
+} from "./serviceKeys.mjs";
 import {isServiceAware} from "./ServicesContext.mjs";
 import {noOp} from "../utils/functional.mjs";
 import winston from "winston";
@@ -8,9 +11,17 @@ export function getEnvNameResolver(serviceAware) {
     return getProvider(serviceAware)[s_envNameResolver]();
 }
 
+const loggerSymbol = Symbol("logger");
+
+export function setLogger(holder, logger) {
+    holder[loggerSymbol] = logger;
+}
+
 export function getLogger(servicesAware) {
     if (isServiceAware(servicesAware)) {
         return getProvider(servicesAware)[s_logger]();
+    } else if (servicesAware[loggerSymbol] !== undefined) {
+        return servicesAware[loggerSymbol];
     } else if (process.env.ZUPFE_LOG_LEVEL) {
         return winston.createLogger({
             level: process.env.ZUPFE_LOG_LEVEL,
