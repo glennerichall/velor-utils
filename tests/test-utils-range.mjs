201e797dd6f5d5d297f800cfe44f625d7424711f
Glenn Hall <glennerichall@hotmail.com>
2025-09-24 19:10:18 -0400

    added Range

---
+++ tests/test-utils-range.mjs
@@ -0,0 +1,361 @@
+import {setupTestContext} from "../test/setupTestContext.mjs";
+import {Range} from "../utils/Range.mjs";
+
+const {test, expect} = setupTestContext();
+
+test.describe('Range', () => {
+
+    test('#first', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        expect(new Range(range).first).to.eq(10);
+    })
+
+    test('#last', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        expect(new Range(range).last).to.eq(100);
+    })
+
+    test('#max', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        expect(new Range(range).max).to.eq(1000);
+    })
+
+    test('#count', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        expect(new Range(range).count).to.eq(91);
+    })
+
+    test('#moveDown 1', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).moveDown();
+        expect(r.first).to.eq(11);
+        expect(r.last).to.eq(101);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#moveDown 2', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).moveDown(2);
+        expect(r.first).to.eq(12);
+        expect(r.last).to.eq(102);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#moveDown 1000', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).moveDown(1000);
+        expect(r.first).to.eq(910);
+        expect(r.last).to.eq(1000);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#moveUp 1', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).moveUp();
+        expect(r.first).to.eq(9);
+        expect(r.last).to.eq(99);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#moveUp 2', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).moveUp(2);
+        expect(r.first).to.eq(8);
+        expect(r.last).to.eq(98);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#moveUp 1000', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).moveUp(1000);
+        expect(r.first).to.eq(0);
+        expect(r.last).to.eq(90);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#jumpToFirst', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).jumpToFirst();
+        expect(r.first).to.eq(0);
+        expect(r.last).to.eq(90);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#jumpToFirst 77', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).jumpToFirst(77);
+        expect(r.first).to.eq(77);
+        expect(r.last).to.eq(167);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#jumpToFirst overflow max', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).jumpToFirst(990);
+        expect(r.first).to.eq(910);
+        expect(r.last).to.eq(1000);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#jumpToFirst overflow min', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).jumpToFirst(-1);
+        expect(r.first).to.eq(0);
+        expect(r.last).to.eq(90);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#jumpToLast', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).jumpToLast();
+        expect(r.first).to.eq(910);
+        expect(r.last).to.eq(1000);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#jumpToLast overflow max', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).jumpToLast(1010);
+        expect(r.first).to.eq(910);
+        expect(r.last).to.eq(1000);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#jumpToLast overflow min', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).jumpToLast(10);
+        expect(r.first).to.eq(0);
+        expect(r.last).to.eq(90);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#jumpToLast 110', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).jumpToLast(110);
+        expect(r.first).to.eq(20);
+        expect(r.last).to.eq(110);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#pageUp', () => {
+        const range = {
+            first: 110,  // inclusive
+            last: 200,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).pageUp();
+        expect(r.first).to.eq(110 - 91);
+        expect(r.last).to.eq(200 - 91);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#pageUp overflow', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).pageUp().pageUp();
+        expect(r.first).to.eq(0);
+        expect(r.last).to.eq(90);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#pageDown', () => {
+        const range = {
+            first: 110,  // inclusive
+            last: 200,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).pageDown();
+        expect(r.first).to.eq(110 + 91);
+        expect(r.last).to.eq(200 + 91);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#pageDown overflow', () => {
+        const range = {
+            first: 900,  // inclusive
+            last: 990,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).pageDown().pageDown();
+        expect(r.first).to.eq(910);
+        expect(r.last).to.eq(1000);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#pageDown.#pageUp', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range).pageDown().pageUp();
+        expect(r.first).to.eq(10);
+        expect(r.last).to.eq(100);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test('#done', () => {
+        const range = {
+            first: 10,  // inclusive
+            last: 100,  // inclusive
+            max: 1000   // inclusive
+        };
+
+        const r = new Range(range)
+            .moveDown(10)
+            .pageDown()
+            .pageDown()
+            .pageDown()
+            .pageUp()
+            .pageUp();
+        expect(r.first).to.eq(111);
+        expect(r.last).to.eq(201);
+        expect(r.max).to.eq(1000);
+        expect(r.count).to.eq(91);
+    })
+
+    test("#expand", () => {
+        const r = new Range({first: 10, last: 99, max: 1000})
+            .expand();
+
+        expect(r).to.be.an('Array');
+        expect(r).to.have.lengthOf(90);
+        expect(r[0]).to.eq(10);
+        expect(r[89]).to.eq(99);
+    })
+
+    test("#toArray", () => {
+        const r = new Range({first: 10, last: 99, max: 1000})
+            .toArray();
+
+        expect(r).to.be.an('Array');
+        expect(r).to.have.lengthOf(3);
+        expect(r[0]).to.eq(10);
+        expect(r[1]).to.eq(99);
+        expect(r[2]).to.eq(1000);
+    })
+
+    test("iterator", () => {
+        let i = 10;
+        for (let val of new Range({first: 10, last: 99, max: 1000})) {
+            expect(val).to.eq(i++);
+        }
+    })
+})
---
+++ utils/Range.mjs
@@ -0,0 +1,141 @@
+export class Range {
+
+    constructor(range) {
+        this._range = range;
+    }
+
+    get first() {
+        return this._range.first;
+    }
+
+    get last() {
+        return this._range.last;
+    }
+
+    get count() {
+        return this.last - this.first + 1;
+    }
+
+    get max() {
+        return this._range.max;
+    }
+
+    static fromArray(arr) {
+        return new Range({
+            first: arr[0],
+            last: arr[1],
+            max: arr[2],
+        });
+    }
+
+    setValue(value) {
+        const current = this._range;
+        const range = {
+            ...current,
+            ...value,
+        };
+
+        if (range.last >= range.max) {
+            const dist = range.last - range.first;
+            range.last = range.max;
+            range.first = range.max - dist;
+        }
+
+        if (current.max !== range.max &&
+            range.first < 0) {
+            const d = -range.first;
+            range.first = 0;
+            range.last += d;
+        }
+
+        if (current.first !== range.first ||
+            current.last !== range.last ||
+            current.max !== range.max) {
+            this._range = {...range};
+        }
+        return this;
+    }
+
+    moveDown(d = 1) {
+        const range = this._range;
+        let dist = range.last - range.first;
+        let first = range.first + d;
+        let last = range.last + d;
+        let max = range.max;
+
+        if (first < 0) {
+            first = 0;
+            last = dist;
+        } else if (last > max) {
+            last = max;
+            first = last - dist;
+        }
+
+        this._range = {
+            first,
+            last,
+            max
+        };
+
+        return this;
+    }
+
+    moveUp(d = 1) {
+        this.moveDown(-d);
+        return this;
+    }
+
+    jumpToFirst(index = 0) {
+        const d = index - this.first;
+        this.moveDown(d);
+        return this;
+    }
+
+    jumpToLast(index) {
+        if (index === undefined) {
+            index = this.max;
+        }
+        const d = index - this.last;
+        this.moveDown(d);
+        return this;
+    }
+
+    pageDown() {
+        const d = this.count;
+        this.moveDown(d);
+        return this;
+    }
+
+    pageUp() {
+        const d = this.count;
+        this.moveUp(d);
+        return this;
+    }
+
+    done() {
+        return this._range;
+    }
+
+    toArray() {
+        return [
+            this.first,
+            this.last,
+            this.max
+        ];
+    }
+
+    expand() {
+        const range = this._range;
+        const res = [];
+        for (let i = range.first; i <= range.last; i++) {
+            res.push(i);
+        }
+        return res;
+    }
+
+    * [Symbol.iterator]() {
+        for (let i = this.first; i <= this.last; i++) {
+            yield i;
+        }
+    }
+}
