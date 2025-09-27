bf26833ca4cfc0d06cfaf8d7c9e2610faeffda7e
Glenn Hall <glennerichall@hotmail.com>
2025-09-27 10:41:57 -0400

    fixed assert

--- utils/assertions.mjs
+++ utils/assertions.mjs
@@ -3,9 +3,15 @@ import {
     ENV_TEST
 } from "../env.mjs";
 
+export class AssertionError extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
+
 export function assert(cond, message) {
-    if (process.env.NODE_END === ENV_DEVELOPMENT ||
+    if (process.env.NODE_ENV === ENV_DEVELOPMENT ||
         process.env.NODE_ENV === ENV_TEST) {
-        if (!cond) throw new Error(message);
+        if (!cond) throw new AssertionError(message);
     }
 }
